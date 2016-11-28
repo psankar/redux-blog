8a2519288dbe008ed00d82bd91b35ddb65b49694
Sankar சங்கர் <sankar.curiosity@gmail.com>
2016-11-28 17:18:23 +0530

    77 index actions

--- src/routes.js
+++ src/routes.js
@@ -4,15 +4,8 @@ import { Route, IndexRoute } from 'react-router';
 import App from './App';
 import PostsIndex from './components/posts_index';
 
-const Greeting = () => {
-    return <div>Hey There!</div>
-}
-
 export default (
     <Route path="/" component={App} >
         <IndexRoute component={PostsIndex} />
-        <Route path="greet" component={Greeting} />
-        <Route path="greet2" component={Greeting} />
-        <Route path="greet3" component={Greeting} />
     </Route>
 );
