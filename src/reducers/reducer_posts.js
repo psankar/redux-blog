5e72a9555a425a3a30492aafa5a4e1d125fa492d
Sankar சங்கர் <sankar.curiosity@gmail.com>
2016-11-28 23:07:12 +0530

    79 posts reducer

--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -5,7 +5,8 @@ const INITIAL_STATE={ all:[], post: null};
 export default function(state=INITIAL_STATE, action) {
     switch (action.type) {
         case FETCH_POSTS:
-
+            return { ...state, all: action.payload.data };
+        default:
+            return state;
     }
-    return state;
 }
