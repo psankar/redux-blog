9a357af4cb560d4ab9f2211e739fdf3a90c7def8
Sankar சங்கர் <sankar.curiosity@gmail.com>
2016-12-05 22:16:49 +0530

    89 form validation

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -3,22 +3,61 @@ import { Field, reduxForm } from 'redux-form';
 
 import { createPost } from '../actions/index';
 
+const renderInput = ({ input, label, type, meta: { touched, error, warning } }) => (
+  <div>
+    <label>{label}</label>
+    <div>
+      <input {...input} placeholder={label} type={type}/>
+      {touched && ((error && <span>{error}</span>) || (warning && <span>{warning}</span>))}
+    </div>
+  </div>
+);
+
+const renderTextArea = ({input, meta: { touched, error, warning }}) => (
+    <div>
+        <label>Content</label>
+        <div>
+            <textarea {...input} placeholder="Content" rows="10" cols="40"></textarea>
+            {touched && ((error && <span>{error}</span>) || (warning && <span>{warning}</span>))}
+        </div>
+    </div>
+);
+
 class PostsNew extends Component {
 
     render() {
         const { handleSubmit, title, categories, content } = this.props;
         return (
             <form onSubmit={handleSubmit(createPost)}>
-                <Field name="title" component="input" type="text" placeholder="Title" {...title} />
-                <Field name="categories" component="input" type="text" placeholder="Categories" {...categories} />
-                <Field name="content" component="textarea" placeholder="Content" {...content} />
+                <Field name="title" component={renderInput} label="Title" type="text" {...title} />
+                <Field name="categories" component={renderInput} label="Categories" type="text" {...categories} />
+                <Field name="content" component={renderTextArea} {...content} />
                 <button type="submit">Submit</button>
             </form>
         );
     }
 }
 
+const validate = values => {
+    const errors = {}
+    if (!values.title) {
+        errors.title = 'Required';
+    }
+    if (!values.categories) {
+        errors.categories = 'Required';
+    }
+    console.log("######", values);
+    console.log("@@@@", values.content);
+    if (!values.content) {
+        errors.content = 'Content cannot be empty';
+    } else if (values.content.length < 3) {
+        errors.content = 'Content should be more than 3 characters';
+    }
+
+    return errors;
+}
 
 export default reduxForm({
-    form: 'NewPostForm'
+    form: 'NewPostForm',
+    validate
 })(PostsNew);
